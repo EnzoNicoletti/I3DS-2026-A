03c326818fdb50172f52bfba3220c94b3bfd3798
EnzoNicoletti <enzo.nicoletti@aluno.senai.br>
2026-02-12 17:01:45 -0300

    feat: adicionar novos componentes de Link, Rodape e SocialLink, e atualizar o componente Perfil

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -1,20 +1,35 @@
 import './App.css'
+import Link from './components/Link/Link';
 import Perfil from './components/Perfil/Perfil'
+import Rodape from './components/Rodape/Rodape';
+import SocialLink from './components/SocialLink/SocialLink';
 function App() {
-
   return (
     <div id="app">
 
-<Perfil />
+       <Perfil fotoPerfil={"https://placehold.co/200"}>Enzo Nicoletti</Perfil>
+       <Perfil fotoPerfil={"https://placehold.co/100"}>Kyderom Goodman</Perfil>
 
        <div className="switch">
         Botão Switch
        </div>
-       <div className="links"></div>
-       <div className="socialLinks"></div>
-       <div className='rodape'></div>
+       <ul>
+       <Link url={""}>Inscreva-se</Link>
+       <Link url={""}>Minha Playlist</Link>
+       <Link url={""}>Me pague um café!</Link>
+       <Link url={""}>Conheça o curso DEV</Link>
+       </ul>
+       <div className="socialLinks">
+        <SocialLink url={"https://github.com/EnzoNicoletti"} icon={"logo-github"}/>
+        <SocialLink url={"https://www.youtube.com/channel/UCW6W5loHJa2Ppf8MLH1MBEA"} icon={"logo-youtube"}/>
+        <SocialLink url={"https://br.linkedin.com/"} icon={"logo-linkedin"}/>
+        <SocialLink url={"https://www.instagram.com/"} icon={"logo-instagram"}/>
+       </div>
+       <div className="rodape">
+        <Rodape>EnzoNicoletti</Rodape>
+       </div>
     </div>
   )
 }
 
-export default App
+export default App;
--- 03-devlinks/src/components/Perfil/Perfil.jsx
+++ 03-devlinks/src/components/Perfil/Perfil.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Perfil = () => {
+const Perfil = ({children, fotoPerfil}) => {
   return (
        <div className="perfil">
-       <img src="https://placehold.co/200x200" alt="perfil" />
-       <p>@Seu Nome</p>
+       <img src={fotoPerfil} alt="perfil" />
+       <p>{children}</p>
        </div>
   )
 }
