8c50993ed0e613c926837135f22c023b7306137c
EnzoNicoletti <enzo.nicoletti@aluno.senai.br>
2026-02-26 08:23:17 -0300

    feat: adicionar os componentes MovieCard e MovieDescription com estilização
    -Criado o componente MovieCard para exibir detalhes do filme e controlar a abertura/fechamento do modal.
    
    -Implementado o componente MovieDescription para buscar e exibir informações detalhadas do filme.
    
    -Adicionados módulos CSS para estilização dos componentes MovieCard e MovieDescription.
    
    -Introduzido o componente Rodape para o rodapé, com link personalizável.
    
    -Adicionados estilos globais em index.css e configurado o Vite para a aplicação React.
    
    -Incluído o asset search.svg para uso futuro na funcionalidade de busca.

---
+++ 04-devflix/src/App.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+import logo from "./assets/devflix.png";
+import lupa from "./assets/search.svg";
+
+import Rodape from "./components/Rodape/Rodape";
+import MovieCard from "./components/MovieCard/MovieCard";
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
+
+  //Utilizando uma CHAVE de API do arquivo .env
+  const apiKey = import.meta.env.VITE_OMDB_API_KEY;
+  const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`;
+
+  //Criando a conexão com a API e trazendo informações
+  const searchMovies = async (title) => {
+    const response = await fetch(`${apiUrl}&s=${title}`);
+    const data = await response.json();
+
+    //Alimentando a variavel movies
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies(""); // termo para pesquina ao carregar o site
+  }, []);
+
+  return (
+    <div id="App">
+      <img
+        id="Logo"
+        src={logo}
+        alt="Logotipo do serviço de streaming Devflix, com letras vermelhas e fundo preto, promovendo conteúdo de séries, filmes e entretenimento online."
+      />
+
+      <div className="search">
+        <input
+          onKeyDown={(e) => e.key === "Enter" && searchMovies(search)}
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          placeholder="Pesquise por filmes"
+        />
+        <img
+          onClick={() => searchMovies(search)}
+          src={lupa}
+          alt="Botão de ação para pesquisa!"
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie, index) => (
+            <MovieCard key={index} {...movie} apiUrl={apiUrl} />
+          ))}
+        </div>
+      ) : (
+        <h2 className="empty">😢 Filme não encontrado 😢</h2>
+      )}
+
+      <Rodape link={"https://github.com/EnzoNicoletti"}>EnzoNicoletti</Rodape>  
+    </div>
+  );
+};
+
+export default App;
